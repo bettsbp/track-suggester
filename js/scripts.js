4fd4d3ab14535ef34332a9c941921a0b31086554
Brandon Betts <bettsb.brandon@gmail.com>
2018-06-01 15:19:04 -0700

    add beginnings to js scripts that actually work with the forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
   $("#track").submit(function(event) {
-    var score = parseInt($("input#track").val());
 
-    if (score === 5) {
+    var scoreOne = parseInt($("input:radio[name=track1]:checked").val());
+    var scoreTwo = parseInt($("input:radio[name=track2]:checked").val());
+    var scoreThree = parseInt($("input:radio[name=track3]:checked").val());
+    var scoreFour = parseInt($("input:radio[name=track4]:checked").val());
+    var scoreFive = parseInt($("input:radio[name=track5]:checked").val());
+
+    if (scoreOne + scoreTwo + scoreThree + scoreFour + scoreFive <= 5) {
       ("#ruby").show();
+      ("#css").hide();
+    } else if (scoreOne + scoreTwo + scoreThree + scoreFour + scoreFive >= 6 ){
+      ("#css").show();
+      ("ruby").hide();
     }
 
     event.preventDefault();
